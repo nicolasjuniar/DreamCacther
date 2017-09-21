9e460f6428783fc7f1bf049c5c57addacbe89af5
Rahmat Al Hakam <rahmatalhakam@gmail.com>
2017-09-21 11:19:39 +0700

    [Rahmat] Fix masalah hardcode utk string, dimen, color; ubag ke desain yang baru, tambah select cateogory, dll

--- app/src/main/java/com/cheteam/dreamcatcher/AddPost/View/EditPostActivity.java
+++ app/src/main/java/com/cheteam/dreamcatcher/AddPost/View/EditPostActivity.java
@@ -1,34 +1,38 @@
 package com.cheteam.dreamcatcher.AddPost.View;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.cheteam.dreamcatcher.ArticlePreview.View.ActivityPreview;
 import com.cheteam.dreamcatcher.ArticlePreview.View.EditPostPreview;
 import com.cheteam.dreamcatcher.R;
 import com.cheteam.dreamcatcher.Timeline.Fragment.DialogFragmentSelectCategory;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Rahmat Al Hakam on 16/09/2017.
- * Layout Edit Post yang sesuai dengan zeplin
  */
 
 public class EditPostActivity extends AppCompatActivity{
     @BindView(R.id.et_title_ep) TextView et_title_ep;
     @BindView(R.id.toolbar_ep) Toolbar myToolbar;
-    @BindView(R.id.tv_add_category_ep) TextView tv_add_category_ep;
-    @BindView(R.id.tv_edit_category_ep) TextView tv_edit_category_ep;
+    @BindView(R.id.ll_select_category_ep) LinearLayout ll_select_category_ep;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,23 +42,24 @@ public class EditPostActivity extends AppCompatActivity{
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle("Edit Post");
         myToolbar.setTitleTextColor(getResources().getColor(R.color.White));
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         myToolbar.setSubtitleTextColor(getResources().getColor(R.color.White));
-
-        tv_add_category_ep = (TextView) findViewById(R.id.tv_add_category_ep);
-        tv_edit_category_ep= (TextView) findViewById(R.id.tv_edit_category_ep);
-
-        tv_add_category_ep.setOnClickListener(new View.OnClickListener(){
+        ll_select_category_ep.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                new DialogFragmentSelectCategory().show(getSupportFragmentManager(),"Select Category");
-            }
-        });
-        tv_edit_category_ep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new DialogFragmentSelectCategory().show(getSupportFragmentManager(),"Select Category");
+                String[] singleChoiceItems = getResources().getStringArray(R.array.dialog_choice_array);;
+                int itemSelected = 0;
+                new AlertDialog.Builder(EditPostActivity.this)
+                        .setTitle("Select Category")
+                        .setSingleChoiceItems(singleChoiceItems, itemSelected, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        })
+                        .setPositiveButton("APPLY",null)
+                        .setNegativeButton("CANCEL", null)
+                        .show();
             }
         });
     }
@@ -69,7 +74,7 @@ public class EditPostActivity extends AppCompatActivity{
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id==R.id.add_post_menu){
-            Intent intent = new Intent(EditPostActivity.this,EditPostPreview.class);
+            Intent intent = new Intent(EditPostActivity.this,ActivityPreview.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/cheteam/dreamcatcher/ArticlePreview/View/ArticlePreview.java
+++ app/src/main/java/com/cheteam/dreamcatcher/ArticlePreview/View/ArticlePreview.java
@@ -1,6 +1,5 @@
 package com.cheteam.dreamcatcher.ArticlePreview.View;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,19 +11,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cheteam.dreamcatcher.AddPost.View.EditPostActivity;
-import com.cheteam.dreamcatcher.ArticlePreview.API.ViewArticleApi;
 import com.cheteam.dreamcatcher.ArticlePreview.Controller.ArticleController;
 import com.cheteam.dreamcatcher.ArticlePreview.Model.ViewArticleResponse;
 import com.cheteam.dreamcatcher.CommentSection.View.MainComment;
 import com.cheteam.dreamcatcher.R;
-import com.cheteam.dreamcatcher.ServiceGenerator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by Rahmat Al Hakam on 09/09/2017.
@@ -39,7 +33,8 @@ public class ArticlePreview extends AppCompatActivity implements ArticleControll
     @BindView(R.id.AvatarUser) CircleImageView AvatarUser;
     @BindView(R.id.bg_post) ImageView bg_post;
     @BindView(R.id.my_toolbar_article_layout) Toolbar myToolbar;
-    ArticleController AC;
+    @BindView(R.id.tv_category_av) TextView tv_category_av;
+    ArticleController mArticleController;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,17 +45,16 @@ public class ArticlePreview extends AppCompatActivity implements ArticleControll
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle("View Post");
         myToolbar.setTitleTextColor(getResources().getColor(R.color.White));
-        android.support.v7.app.ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         myToolbar.setSubtitleTextColor(getResources().getColor(R.color.White));
 
-        AC=new ArticleController(this);
-        AC.GetArticle(1);
+        mArticleController =new ArticleController(this);
+        mArticleController.GetArticle(1);
 
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.article_layout, menu);
 
         return true;
@@ -68,9 +62,6 @@ public class ArticlePreview extends AppCompatActivity implements ArticleControll
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         if(id==R.id.comment_action_bar)
@@ -106,50 +97,55 @@ public class ArticlePreview extends AppCompatActivity implements ArticleControll
             tv_name.setText(model.name);
             tanggal_post.setText(model.published_at);
             tv_content_article.setText(model.content);
-            if(model.id_background==1){
-                bg_post.setBackgroundResource(R.drawable.red_bg);
+            tv_category_av.setText(model.categories);
+            switch (model.id_background){
+                case 1:
+                    bg_post.setBackgroundResource(R.drawable.red_bg);
+                    break;
+                case 2:
+                    bg_post.setBackgroundResource(R.drawable.green_bg);
+                    break;
+                case 3:
+                    bg_post.setBackgroundResource(R.drawable.blue_bg);
+                    break;
+                case 4:
+                    bg_post.setBackgroundResource(R.drawable.yellow_bg);
+                    break;
+                case 5:
+                    bg_post.setBackgroundResource(R.drawable.violet_bg);
+                    break;
             }
-            if(model.id_background==2){
-                bg_post.setBackgroundResource(R.drawable.green_bg);
-            }
-            if(model.id_background==3){
-                bg_post.setBackgroundResource(R.drawable.blue_bg);
-            }
-            if(model.id_background==4){
-                bg_post.setBackgroundResource(R.drawable.yellow_bg);
-            }
-            if(model.id_background==5){
-                bg_post.setBackgroundResource(R.drawable.violet_bg);
-            }
-            if(model.id_avatar==0){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_0);
-            }
-            if(model.id_avatar==1){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_1);
-            }
-            if(model.id_avatar==2){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_2);
-            }
-            if(model.id_avatar==3){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_3);
-            }
-            if(model.id_avatar==4){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_4);
-            }
-            if(model.id_avatar==5){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_5);
-            }
-            if(model.id_avatar==6){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_6);
-            }
-            if(model.id_avatar==7){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_7);
-            }
-            if(model.id_avatar==8){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_8);
-            }
-            if(model.id_avatar==9){
-                AvatarUser.setBackgroundResource(R.drawable.avatar_9);
+            switch (model.id_avatar){
+                case 0:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_0);
+                    break;
+                case 1:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_1);
+                    break;
+                case 2:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_2);
+                    break;
+                case 3:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_3);
+                    break;
+                case 4:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_4);
+                    break;
+                case 5:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_5);
+                    break;
+                case 6:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_6);
+                    break;
+                case 7:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_7);
+                    break;
+                case 8:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_8);
+                    break;
+                case 9:
+                    AvatarUser.setBackgroundResource(R.drawable.avatar_9);
+                    break;
             }
         }
     }
